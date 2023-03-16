98a42524bb813cc573c8b6f0f57e339fd6b37d59
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2023-03-16 16:08:16 +0900

    [docs]Remove Menu Groups

--- src/menu-items/dashboard.js
+++ src/menu-items/dashboard.js
@@ -1,22 +1,99 @@
 // assets
-import { IconDashboard } from '@tabler/icons';
+import { IconDashboard, IconBrandChrome, IconHelp, IconKey, IconTypography, IconPalette, IconShadow, IconWindmill } from '@tabler/icons';
 
 // constant
-const icons = { IconDashboard };
+const icons = { IconDashboard, IconBrandChrome, IconHelp, IconKey, IconTypography, IconPalette, IconShadow, IconWindmill };
 
 // ==============================|| DASHBOARD MENU ITEMS ||============================== //
 
 const dashboard = {
     id: 'dashboard',
-    title: 'Dashboard',
+    title: '대시보드',
     type: 'group',
     children: [
         {
             id: 'default',
-            title: 'Dashboard',
+            title: '대시보드',
             type: 'item',
             url: '/dashboard/default',
             icon: icons.IconDashboard,
+            breadcrumbs: false
+        },
+        {
+            id: 'authentication',
+            title: 'Authentication',
+            type: 'collapse',
+            icon: icons.IconKey,
+
+            children: [
+                {
+                    id: 'login3',
+                    title: 'Login',
+                    type: 'item',
+                    url: '/pages/login/login3',
+                    target: true
+                },
+                {
+                    id: 'register3',
+                    title: 'Register',
+                    type: 'item',
+                    url: '/pages/register/register3',
+                    target: true
+                }
+            ]
+        },
+        {
+            id: 'util-typography',
+            title: 'Typography',
+            type: 'item',
+            url: '/utils/util-typography',
+            icon: icons.IconTypography,
+            breadcrumbs: false
+        },
+        {
+            id: 'util-color',
+            title: 'Color',
+            type: 'item',
+            url: '/utils/util-color',
+            icon: icons.IconPalette,
+            breadcrumbs: false
+        },
+        {
+            id: 'util-shadow',
+            title: 'Shadow',
+            type: 'item',
+            url: '/utils/util-shadow',
+            icon: icons.IconShadow,
+            breadcrumbs: false
+        },
+        {
+            id: 'icons',
+            title: 'Icons',
+            type: 'collapse',
+            icon: icons.IconWindmill,
+            children: [
+                {
+                    id: 'tabler-icons',
+                    title: 'Tabler Icons',
+                    type: 'item',
+                    url: '/icons/tabler-icons',
+                    breadcrumbs: false
+                },
+                {
+                    id: 'material-icons',
+                    title: 'Material Icons',
+                    type: 'item',
+                    url: '/icons/material-icons',
+                    breadcrumbs: false
+                }
+            ]
+        },
+        {
+            id: 'sample-page',
+            title: 'Sample Page',
+            type: 'item',
+            url: '/sample-page',
+            icon: icons.IconBrandChrome,
             breadcrumbs: false
         }
     ]
